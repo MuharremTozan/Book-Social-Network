3e13a680af84e6c470010a4bee85d5bf3bf6d796
Muharrem Tozan <muharremtozan@hotmail.com>
2024-07-19 18:00:41 +0300

    Junit-Jupiter-Api broken

--- src/main/java/com/book/booknetwork/email/EmailTemplateName.java
+++ src/main/java/com/book/booknetwork/email/EmailTemplateName.java
@@ -1,2 +1,17 @@
-package com.book.booknetwork.email;public enum EmailTemplateName {
+package com.book.booknetwork.email;
+
+import lombok.Getter;
+
+@Getter
+public enum EmailTemplateName {
+
+    ACTIVATE_ACCOUNT("activate_account")
+
+    ;
+    private final String name;
+
+    EmailTemplateName(String name) {
+        this.name = name;
+    }
+
 }
--- src/main/java/com/book/booknetwork/security/UserDetailsServiceImpl.java
+++ src/main/java/com/book/booknetwork/security/UserDetailsServiceImpl.java
@@ -1,2 +1,24 @@
-package com.book.booknetwork.security;public class UserDetailsServiceImpl {
+package com.book.booknetwork.security;
+
+import com.book.booknetwork.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+
+    private final UserRepository repository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
 }
